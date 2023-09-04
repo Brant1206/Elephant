34cf6e65f3382ba461f1ec861a84ec1ed6e8376d
BRANT\stop1 <stop12062002@hotmail.com.tw>
2023-09-04 23:20:53 +0800

    加入專案檔案。

---
+++ Elephant/Elephant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elephant
+{
+    class Elephant
+    {
+        public int EarSize;
+        public string Name;
+
+        public void WhoAmI(string name, int earSize)
+        {
+            Console.WriteLine("Calling " + name + ".WhoAmI()");
+            Console.WriteLine("My name is " + name + ".");
+            Console.WriteLine("My ears are " + earSize + " inches tall.");
+        }
+    }
+}
---
+++ Elephant/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Elephant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elephant Lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+            Elephant Lucinda = new Elephant() { Name = "Lloyd", EarSize = 33 };
+
+            Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+            while (true)
+            {
+                Console.Write("You pressed ");
+                string press = Console.ReadLine();
+                if (int.TryParse(press, out int value))
+                {
+                    if (value == 1)
+                    {
+                        Lloyd.WhoAmI(Lloyd.Name, Lloyd.EarSize);
+                    }
+                    else if (value == 2)
+                    {
+                        Lucinda.WhoAmI(Lucinda.Name, Lucinda.EarSize);
+                    }
+                    else if (value == 3)
+                    {
+                        Elephant Swap;
+                        Swap = Lloyd;
+                        Lloyd = Lucinda;
+                        Lucinda = Swap;
+                        Console.WriteLine("References have been swapped");
+                    }
+                    else return;
+                }
+                else return;
+            }
+        }
+    }
+}
